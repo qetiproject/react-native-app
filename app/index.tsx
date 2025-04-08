f16d16e74f81920e24e7b01244354116008c493a
Qeti Khetsuriani <ketideveloper@gmail.com>
2025-04-08 15:04:27 +0400

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function Index() {
+  
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Main Page</Text>
+    </SafeAreaView>
+  );
+}
