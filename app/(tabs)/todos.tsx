ffc899aea141bb03b18f450e063c455b066c56f2
Qeti Khetsuriani <ketideveloper@gmail.com>
2025-04-09 21:07:47 +0400

    props

--- app/(tabs)/todos.tsx
+++ app/(tabs)/todos.tsx
@@ -1,34 +1,10 @@
 import React, { useEffect, useState} from "react";
 import { View, Text, FlatList } from "react-native"
 import { CommentService, DataService } from "../services/data.service";
-
-interface Data {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
-
-interface Comment {
-    postId: number,
-    id: number,
-    name: string,
-    email: string,
-    body: string;
-}
+import Comments from "../comments";
 
 const Todos = () => {
-    const [data, setData] = useState<Data[]>([])
-    const [comment, setComment] = useState<Comment[]>([])
-
-    const getDataTask = async () => {
-        try {
-            const response = await DataService();
-            setData(response)
-        }catch(error) {
-            console.log(error, "error")
-        }
-    }
+    const [comment, setComment] = useState<any>({})
 
     const getComments = async () => {
        try{
@@ -40,33 +16,13 @@ const Todos = () => {
     }
 
     useEffect(() => {
-        getDataTask();
         getComments();
     }, [])
 
     return (
         <View>
-            <FlatList
-                data={data}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) =>(
-                    <View>
-                        <Text>{item.title} - title</Text>
-                    </View>
-                )}
-            ></FlatList>
             <Text>Comments Data</Text>
-            <FlatList
-                data={comment}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => (
-                    <View>
-                        <Text>{item.email}</Text>
-                        <Text>{item.name}</Text>
-                        <Text>{item.body}</Text>
-                    </View>
-                )}
-            ></FlatList>
+            <Comments data={comment} />
         </View>
     )
 }
---
+++ app/comments.tsx
@@ -0,0 +1,27 @@
+import { FlatList, View, Text } from "react-native"
+
+export interface Comment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
+
+const Comments = ({data}: any) => {
+    return (
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => (
+                <View>
+                    <Text>{item.email}</Text>
+                    <Text>{item.name}</Text>
+                    <Text>{item.body}</Text>
+                </View>
+            )}
+        ></FlatList>
+    )
+}
+
+export default Comments
